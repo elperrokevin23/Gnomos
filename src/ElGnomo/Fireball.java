bf9ceefb375749c83541d8206f7e0f248f65406c
elperrokevin23 <rodrigoaranda609@gmail.com>
2024-10-25 15:05:45 -0300

    Update Fireball.java

--- src/ElGnomo/Fireball.java
+++ src/ElGnomo/Fireball.java
@@ -10,13 +10,15 @@ public class Fireball {
     private int angulo;
     private double velocidad; // velocidad de la bola de fuego
     private boolean moviendoDerecha; // dirección de movimiento
+    private double ancho;
+    private double alto;
 
     public Fireball(double x, double y, int angulo, boolean moviendoDerecha) {
         this.x = x;
         this.y = y;
         this.angulo = angulo;
         this.moviendoDerecha = moviendoDerecha;
-        this.velocidad = 1.5; // Ajusta la velocidad de la bola de fuego
+        this.velocidad = 5; // Ajusta la velocidad de la bola de fuego
     }
 
     // Método para mover la bola de fuego
@@ -30,7 +32,7 @@ public class Fireball {
 
     // Método para dibujar la bola de fuego en pantalla
     public void dibujar(Entorno entorno) {
-        entorno.dibujarRectangulo(this.x, this.y, 20, 20, 0, Color.ORANGE);
+        entorno.dibujarRectangulo(this.x, this.y, 50, 50, 0, Color.ORANGE);
     }
 
     // Métodos getters
@@ -41,4 +43,31 @@ public class Fireball {
     public double getY() {
         return this.y;
     }
+    public boolean chocoIzquierda(Entorno e) {
+		return x <= 0;
+	}
+    public double getAncho() {
+        return ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+    public boolean chocoTortuga(Tortugas[] tortuga) {
+		for (int x = 0;x<tortuga.length;x++) {
+			if (tortuga[x] != null
+					&& tortuga[x].getX() <= this.x + this.ancho / 2
+					&& tortuga[x].getX() >= this.x - this.ancho / 2
+					&& (tortuga[x].getY() >= this.y - alto / 2 && tortuga[x].getY() <= this.y
+							+ alto / 2)) {
+				System.out.println("colision");
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean chocoDerecha(Entorno e) {
+		return x >= e.ancho();
+	}
 }
